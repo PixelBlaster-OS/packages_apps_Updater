c3624a4130d21bb649089b7da39964a6d5606f06
satyamrathirar <satyam.rathi47@gmail.com>
2023-10-01 00:17:32 +0530

    added BlasterOS copyright

--- app/src/main/java/com/blaster/Updater/UpdateImporter.java
+++ app/src/main/java/com/blaster/Updater/UpdateImporter.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2020 Shift GmbH
+ * Copyright (C) 2017-2022 The LineageOS Project
+ * Copyright (C) BlasterOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
